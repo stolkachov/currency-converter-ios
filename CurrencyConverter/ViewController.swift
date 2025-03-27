4daa90f57c42d8bb25eb152350912631f028385f
Semen Tolkachov <stolkachov@icloud.com>
2025-03-27 10:28:00 +0100

    Update project settings to use UIKit instead of SwiftUI

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Semen Tolkachov on 27/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
