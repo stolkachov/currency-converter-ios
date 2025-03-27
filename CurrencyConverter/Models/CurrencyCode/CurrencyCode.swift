6ceee8ea2a90d93e500d47fdc52c53b9ad82d53c
Semen Tolkachov <stolkachov@icloud.com>
2025-03-27 08:01:17 +0100

    Add CurrencyCode

---
+++ CurrencyConverter/Models/CurrencyCode/CurrencyCode.swift
@@ -0,0 +1,12 @@
+//
+//  CurrencyCode.swift
+//  CurrencyConverter
+//
+//  Created by Semen Tolkachov on 27/03/2025.
+//
+
+enum CurrencyCode: String {
+    case EUR = "EUR"
+    case PLN = "PLN"
+    case USD = "USD"
+}
