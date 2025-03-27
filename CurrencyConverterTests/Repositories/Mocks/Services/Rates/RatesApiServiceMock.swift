ccc7c350c41af156f57ffb2ed2fc986431ac2d01
Semen Tolkachov <stolkachov@icloud.com>
2025-03-27 08:47:44 +0100

    Add RatesApiServiceMock

---
+++ CurrencyConverterTests/Repositories/Mocks/Services/Rates/RatesApiServiceMock.swift
@@ -0,0 +1,31 @@
+//
+//  RatesApiServiceMock.swift
+//  CurrencyConverterTests
+//
+//  Created by Semen Tolkachov on 27/03/2025.
+//
+
+@testable import CurrencyConverter
+
+final class RatesApiServiceMock: RatesApiServiceProtocol {
+    private(set) var fetchRateCalled = false
+    private(set) var fetchRateFromAmount: Double!
+    private(set) var fetchRateFromCurrency: String!
+    private(set) var fetchRateToCurrency: String!
+    private(set) var fetchRateCompletion: (Result<MoneyDto, Error>)?
+
+    func fetchRate(
+        fromAmount: Double,
+        fromCurrency: String,
+        toCurrency: String,
+        completion: @escaping (Result<MoneyDto, Error>) -> Void
+    ) {
+        fetchRateCalled = true
+        fetchRateFromAmount = fromAmount
+        fetchRateFromCurrency = fromCurrency
+        fetchRateToCurrency = toCurrency
+        if let fetchRateCompletion {
+            completion(fetchRateCompletion)
+        }
+    }
+}
