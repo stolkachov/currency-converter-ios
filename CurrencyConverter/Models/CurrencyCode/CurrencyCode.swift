97ee26cb0516925b640dcd3677f763ac38d113ef
Semen Tolkachov <stolkachov@icloud.com>
2025-03-29 07:43:22 +0100

    Add more currencies

--- CurrencyConverter/Models/CurrencyCode/CurrencyCode.swift
+++ CurrencyConverter/Models/CurrencyCode/CurrencyCode.swift
@@ -5,8 +5,29 @@
 //  Created by Semen Tolkachov on 27/03/2025.
 //
 
-enum CurrencyCode: String {
+enum CurrencyCode: String, CaseIterable {
+    case AUD = "AUD"
+    case BRL = "BRL"
+    case CAD = "CAD"
+    case CHF = "CHF"
+    case CNY = "CNY"
     case EUR = "EUR"
-    case PLN = "PLN"
+    case GBP = "GBP"
+    case HKD = "HKD"
+    case IDR = "IDR"
+    case INR = "INR"
+    case JPY = "JPY"
+    case KRW = "KRW"
+    case MXN = "MXN"
+    case MYR = "MYR"
+    case NOK = "NOK"
+    case NZD = "NZD"
+    case RUB = "RUB"
+    case SEK = "SEK"
+    case SGD = "SGD"
+    case THB = "THB"
+    case TRY = "TRY"
+    case TWD = "TWD"
     case USD = "USD"
+    case ZAR = "ZAR"
 }
